2c141e93451262d1ca3ba7c7e48272b077287995
Etienne Bonvin <etienne.bonvin@epfl.ch>
2017-10-05 17:01:22 +0200

    Welcome screen added with fade out to login screen

--- GroupUp/app/src/main/java/ch/epfl/sweng/groupup/activity/main/MainActivity.java
+++ GroupUp/app/src/main/java/ch/epfl/sweng/groupup/activity/main/MainActivity.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.groupup.activity.main;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import ch.epfl.sweng.groupup.R;
@@ -14,11 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        switchToLoginContentView(); // Make it depend on states "LOGGED_IN" and "LOGGED_OUT".
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                switchToLoginContentView(); // Make it depend on states "LOGGED_IN" and "LOGGED_OUT".
+            }
+
+        }, 3000); // 5000ms delay
     }
 
     private void switchToLoginContentView() {
         Intent intent = new Intent(this, LogInActivity.class);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         startActivity(intent);
     }
 }
