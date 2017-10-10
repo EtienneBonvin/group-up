dd5c95a38f3c262302652183c71a0c231253768d
Alix Jeannerot <alix.jeannerot@epfl.ch>
2017-10-10 16:16:47 +0200

    javadoc

--- app/src/main/java/ch/epfl/sweng/groupup/object/account/Account.java
+++ app/src/main/java/ch/epfl/sweng/groupup/object/account/Account.java
@@ -12,7 +12,7 @@ public final class Account {
 
     private static final String NO_FIRST_NAME = "NO_FIRST_NAME";
     private static final String NO_LAST_NAME = "NO_LAST_NAME";
-    private static final String NO_EMAIL = "NO_EMAIL";
+    private static final String NO_EMAIL = "default@defaul.default";
 
     public static Account shared = new Account(NO_FIRST_NAME, NO_LAST_NAME, NO_EMAIL,
             null, new ArrayList<Event>(), new ArrayList<Event>());
@@ -34,42 +34,81 @@ public final class Account {
         this.futureEvents = future;
     }
 
+    /**
+     * Getter for the first name of the account
+     * @return String first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Getter for the last name of the account
+     * @return String last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Getter for the email of the account
+     * @return String email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Getter for the current event of the account
+     * @return Event current
+     */
     public Event getCurrentEvent() {
         return currentEvent;
     }
 
+    /**
+     * Getter for the list of past events
+     * @return List<Event> last events
+     */
     public List<Event> getPastEvents() {
         return pastEvents;
     }
 
+    /**
+     * Getter for the list of future events
+     * @return List<Event> future events
+     */
     public List<Event> getFutureEvents() {
         return futureEvents;
     }
 
+    /**
+     * Change the first name of the shared account
+     * @param firstName
+     * @return the modified shared account, so that it is easier to call in chain
+     */
     public Account withFirstName(String firstName) {
         shared = new Account(firstName, shared.getLastName(), shared.getEmail(),
                 shared.currentEvent, shared.pastEvents, shared.futureEvents);
         return shared;
     }
 
+    /**
+     * Change the last name of the shared account
+     * @param lastName
+     * @return the modified shared account, so that it is easier to call in chain
+     */
     public Account withLastName(String lastName) {
         shared = new Account(shared.getFirstName(), lastName, shared.getEmail(),
                 shared.currentEvent, shared.pastEvents, shared.futureEvents);
         return shared;
     }
 
+    /**
+     * Change the email of the shared account
+     * @param email
+     * @return the modified shared account, so that it is easier to call in chain
+     */
     public Account withEmail(String email) {
         if (emailCheck(email)){
             shared = new Account(shared.getFirstName(), shared.getLastName(), email,
@@ -80,6 +119,12 @@ public final class Account {
 
     }
 
+    /**
+     * Change the current event of the shared account
+     * @param current event
+     * @return the modified shared account, so that it is easier to call in chain
+     */
+
     public Account withCurrentEvent(Event current) {
         if (current.getEventStatus().equals(EventStatus.CURRENT)) {
             shared = new Account(shared.getFirstName(), shared.getLastName(), shared.getEmail(),
@@ -88,18 +133,32 @@ public final class Account {
         } else throw new IllegalArgumentException("Event is not "+ EventStatus.CURRENT.toString());
     }
 
+    /**
+     * Change the past event list of the shared account
+     * @param past event list
+     * @return the modified shared account, so that it is easier to call in chain
+     */
     public Account withPastEvents(List<Event> past) {
         shared = new Account(shared.getFirstName(), shared.getLastName(), shared.getEmail(),
                 shared.getCurrentEvent(), past, shared.getFutureEvents());
         return shared;
     }
 
+    /**
+     * Change the future event list of the shared account
+     * @param future event list
+     * @return the modified shared account, so that it is easier to call in chain
+     */
     public Account withFutureEvents(List<Event> future) {
         shared = new Account(shared.getFirstName(), shared.getLastName(), shared.getEmail(),
                 shared.getCurrentEvent(), shared.getPastEvents(), future);
         return shared;
     }
-
+    /**
+     * Add a past event list of the shared account
+     * @param past event to add
+     * @return the modified shared account, so that it is easier to call in chain
+     */
     public Account addPastEvent(Event past) {
         if (past.getEventStatus().equals(EventStatus.PAST)) {
             List<Event> newPast = new ArrayList<>(shared.getPastEvents());
@@ -109,6 +168,11 @@ public final class Account {
         } else throw new IllegalArgumentException("Event is not "+ EventStatus.PAST.toString());
     }
 
+    /**
+     * Add a future event list of the shared account
+     * @param future event to add
+     * @return the modified shared account, so that it is easier to call in chain
+     */
     public Account addFutureEvent(Event future) {
         if (future.getEventStatus().equals(EventStatus.FUTURE)) {
             List<Event>newFuture = new ArrayList<>(shared.getFutureEvents());
@@ -117,7 +181,10 @@ public final class Account {
             return shared;
         } else throw new IllegalArgumentException("Event is not "+ EventStatus.FUTURE.toString());
     }
-
+    /**
+     * Clear the shared account
+     * @return a cleared shared account
+     */
     public Account clear() {
         shared = new Account(NO_FIRST_NAME, NO_LAST_NAME, NO_EMAIL, null,
                 new ArrayList<Event>(), new ArrayList<Event>());
@@ -154,6 +221,10 @@ public final class Account {
                 '}';
     }
 
+    /**
+     * Print a shorter version of the account with just the names the email and the current event
+     * @return string containing basic informations about an account
+     */
     public String toStringShort() {
         return "Account{" +
                 "firstName='" + firstName + '\'' +
