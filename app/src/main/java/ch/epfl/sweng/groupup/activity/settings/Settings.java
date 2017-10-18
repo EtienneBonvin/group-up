dc918ab357f6c6613377e35d2e178f51ddf56e50
Etienne Bonvin <etienne.bonvin@epfl.ch>
2017-10-18 18:56:37 +0200

    Settings page added accessible from the navigation bar

---
+++ app/src/main/java/ch/epfl/sweng/groupup/activity/settings/Settings.java
@@ -0,0 +1,36 @@
+package ch.epfl.sweng.groupup.activity.settings;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import ch.epfl.sweng.groupup.R;
+import ch.epfl.sweng.groupup.activity.eventListing.EventListingActivity;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        findViewById(R.id.icon_access_group_list)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), EventListingActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+        findViewById(R.id.icon_access_settings)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Settings.class);
+                        startActivity(intent);
+                    }
+                });
+    }
+}
