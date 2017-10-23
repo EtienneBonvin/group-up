373c478bd28e6107a8122a30fc0ef0048259b436
Etienne Bonvin <etienne.bonvin@epfl.ch>
2017-10-23 13:35:21 +0200

    Test changed to avoid at maximum the problem with the date set

--- app/src/androidTest/java/ch/epfl/sweng/groupup/eventCreation/EventCreationTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/groupup/eventCreation/EventCreationTest.java
@@ -1,6 +1,5 @@
 package ch.epfl.sweng.groupup.eventCreation;
 
-import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 
 import org.joda.time.LocalDateTime;
@@ -12,7 +11,6 @@ import java.util.List;
 
 import ch.epfl.sweng.groupup.R;
 import ch.epfl.sweng.groupup.activity.eventCreation.EventCreation;
-import ch.epfl.sweng.groupup.activity.main.MainActivity;
 import ch.epfl.sweng.groupup.lib.Optional;
 import ch.epfl.sweng.groupup.lib.database.Database;
 import ch.epfl.sweng.groupup.object.account.Account;
@@ -21,7 +19,6 @@ import ch.epfl.sweng.groupup.object.event.Event;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
@@ -42,9 +39,6 @@ public class EventCreationTest {
 
         Database.setUpDatabase();
 
-        LocalDateTime start = LocalDateTime.now().plusMinutes(5);
-        LocalDateTime end = LocalDateTime.now().plusMinutes(6);
-
         Member emptyMember = new Member(Optional.<String>empty(), Optional.<String>empty(), Optional.<String>empty(),
                 Optional.<String>empty(), Optional.<String>empty());
         List<Member> expectedMembers = new ArrayList<>();
@@ -55,8 +49,6 @@ public class EventCreationTest {
         expectedMembers.add(emptyMember.withUUID("4"));
         expectedMembers.add(emptyMember.withUUID("5"));
         expectedMembers.add(emptyMember.withUUID(Account.shared.getUUID().getOrElse("Default UUID")));
-
-        Event expected = new Event("My event", start, end, "", expectedMembers);
 
         onView(withId(R.id.ui_edit_event_name)).perform(typeText("My event"));
         onView(withId(R.id.edit_text_add_member)).perform(typeText("0"));
@@ -76,6 +68,11 @@ public class EventCreationTest {
         List<Event> accountEvents = Account.shared.getFutureEvents();
         Event found = new Event("", LocalDateTime.now(), LocalDateTime.now(), "",
                 new ArrayList<Member>());
+
+        LocalDateTime start = LocalDateTime.now().plusMinutes(5);
+        LocalDateTime end = LocalDateTime.now().plusMinutes(6);
+        Event expected = new Event("My event", start, end, "", expectedMembers);
+
         for(Event e : accountEvents){
             if(e.getEventName().equals("My event")){
                 found = e;
@@ -85,6 +82,5 @@ public class EventCreationTest {
 
         assert(found.equals(expected));
     }
-
 }
 
