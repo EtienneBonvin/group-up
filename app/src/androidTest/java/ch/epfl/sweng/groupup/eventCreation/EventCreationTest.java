f25e77b2fd80a81e6c84f73155f743878183e8d1
Etienne Bonvin <etienne.bonvin@epfl.ch>
2017-10-24 10:07:06 +0200

    Test added for event creation, build.gradle updated to allow picker tests

--- app/src/androidTest/java/ch/epfl/sweng/groupup/eventCreation/EventCreationTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/groupup/eventCreation/EventCreationTest.java
@@ -1,7 +1,12 @@
 package ch.epfl.sweng.groupup.eventCreation;
 
+import android.support.test.espresso.contrib.PickerActions;
 import android.support.test.rule.ActivityTestRule;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
+import org.hamcrest.Matchers;
+import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,7 +24,9 @@ import ch.epfl.sweng.groupup.object.event.Event;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressBack;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 public class EventCreationTest {
@@ -51,22 +58,37 @@ public class EventCreationTest {
         expectedMembers.add(emptyMember.withUUID(Account.shared.getUUID().getOrElse("Default UUID")));
 
         addEventName("My event");
+
+        onView(withId(R.id.button_start_date)).perform(pressBack());
+
+        LocalDateTime start = LocalDateTime.now().plusHours(1).plusMinutes(5);
+        LocalDateTime end = LocalDateTime.now().plusHours(1).plusMinutes(6);
+
+        onView(withId(R.id.button_start_date)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
+                .perform(PickerActions.setDate(start.getYear(), start.getMonthOfYear(), start.getDayOfYear()));
+        onView(withId(android.R.id.button1)).perform(click());
+
+        onView(withId(R.id.button_end_date)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
+                .perform(PickerActions.setDate(end.getYear(), end.getMonthOfYear(), end.getDayOfYear()));
+        onView(withId(android.R.id.button1)).perform(click());
+
+        onView(withId(R.id.button_start_time)).perform(click());
+        onView(withClassName(Matchers.equalTo(TimePicker.class.getName())))
+                .perform(PickerActions.setTime(start.getHourOfDay(), start.getMinuteOfHour()));
+        onView(withId(android.R.id.button1)).perform(click());
+
+        onView(withId(R.id.button_end_time)).perform(click());
+        onView(withClassName(Matchers.equalTo(TimePicker.class.getName())))
+                .perform(PickerActions.setTime(end.getHourOfDay(), end.getMinuteOfHour()));
+        onView(withId(android.R.id.button1)).perform(click());
+
         addMembers();
         onView(withId(R.id.save_button)).perform(click());
-
-        List<Event> accountEvents = Account.shared.getFutureEvents();
-        Event found = null;
-
-        LocalDateTime start = LocalDateTime.now().plusMinutes(5);
-        LocalDateTime end = LocalDateTime.now().plusMinutes(6);
         Event expected = new Event("My event", start, end, "", expectedMembers);
 
-        for(Event e : accountEvents){
-            if(e.getEventName().equals("My event")){
-                found = e;
-                break;
-            }
-        }
+        Event found = findEvent();
 
         assert(found.equals(expected));
     }
@@ -77,7 +99,54 @@ public class EventCreationTest {
         addEventName("");
         addMembers();
         onView(withId(R.id.save_button)).perform(click());
+        assert(findEvent() == null);
+    }
 
+    @Test
+    public void noEventCreationOnPastStartDate(){
+        LocalDateTime now = LocalDateTime.now();
+        boolean carry = now.getDayOfMonth() == 0;
+        int day = carry ? 28 : now.getDayOfMonth() - 1;
+        int month = now.getMonthOfYear() - 1;
+        int year = now.getYear();
+        if(carry){
+            carry = now.getMonthOfYear() == 0;
+            month = carry ? 12 : month;
+            if(carry){
+                year --;
+            }
+        }
+        addEventName("My event");
+        onView(withId(R.id.button_start_date)).perform(pressBack());
+        onView(withId(R.id.button_start_date)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
+                .perform(PickerActions.setDate(year, month, day));
+        assert(findEvent() == null);
+    }
+
+    @Test
+    public void noEventCreationOnEndDateBeforeStartDate(){
+        LocalDateTime now = LocalDateTime.now();
+        boolean carry = now.getDayOfMonth() == 0;
+        int day = carry ? 28 : now.getDayOfMonth() - 1;
+        int month = now.getMonthOfYear() - 1;
+        int year = now.getYear();
+        if(carry){
+            carry = now.getMonthOfYear() == 0;
+            month = carry ? 12 : month;
+            if(carry){
+                year --;
+            }
+        }
+        addEventName("My event");
+        onView(withId(R.id.button_end_date)).perform(pressBack());
+        onView(withId(R.id.button_end_date)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
+                .perform(PickerActions.setDate(year, month, day));
+        assert(findEvent() == null);
+    }
+
+    private Event findEvent(){
         List<Event> accountEvents = Account.shared.getFutureEvents();
         Event found = null;
         for(Event e : accountEvents){
@@ -86,7 +155,7 @@ public class EventCreationTest {
                 break;
             }
         }
-        assert(found == null);
+        return found;
     }
 
     private void addMembers(){
