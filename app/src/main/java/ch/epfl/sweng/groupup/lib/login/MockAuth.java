faa2bb6e10854bac995f74d860c99c61d46836ea
CedMaire <cedric.maire@epfl.ch>
2017-10-26 17:27:17 +0200

    Implemented MockAuth class to mock firebase sign in

--- app/src/main/java/ch/epfl/sweng/groupup/lib/login/MockAuth.java
+++ app/src/main/java/ch/epfl/sweng/groupup/lib/login/MockAuth.java
@@ -2,19 +2,49 @@ package ch.epfl.sweng.groupup.lib.login;
 
 import android.content.Intent;
 
+import ch.epfl.sweng.groupup.object.account.Account;
+
 public final class MockAuth implements GoogleAuthenticationService {
+
+    private LoginActivityInterface activityInterface;
+    private boolean signInSuccess;
+    private boolean signOutSuccess;
+
+    public MockAuth(LoginActivityInterface activityInterface, boolean signInSuccess, boolean
+            signOutSuccess) {
+        this.activityInterface = activityInterface;
+        this.signInSuccess = signInSuccess;
+        this.signOutSuccess = signOutSuccess;
+    }
+
     @Override
     public void signIn() {
+        if (signInSuccess) {
+            Account.shared
+                    .withEmail("xavier@maire.com")
+                    .withDisplayName("Cedric")
+                    .withFamilyName("Jeannerot")
+                    .withGivenName("Selma")
+                    .withUUID("EtienneID");
 
+            activityInterface.onSuccess();
+        } else {
+            activityInterface.onFail();
+        }
     }
 
     @Override
     public void signOut() {
-
+        if (signOutSuccess) {
+            Account.shared.clear();
+            activityInterface.onSuccess();
+        } else {
+            activityInterface.onFail();
+        }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
+        // UNUSED
     }
 }
