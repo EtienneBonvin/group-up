f28a7fcb7f25626be25a2ee3389bba402f4c78f7
Xavier Pantet <xavpantet@gmail.com>
2017-10-31 17:56:58 +0100

    Added JavaDoc to MailService

--- app/src/main/java/ch/epfl/sweng/groupup/lib/email/MailService.java
+++ app/src/main/java/ch/epfl/sweng/groupup/lib/email/MailService.java
@@ -7,6 +7,15 @@ import java.util.List;
  * for different purposes everywhere in the app
  */
 public interface MailService {
+    /**
+     * Sends a predefined invitation email to every email address in the list
+     * @param addresses the list of addresses to send the mail to
+     */
     void sendInvitationEmail(List<String> addresses);
+
+    /**
+     * Sends a predefined invitation email to the given address
+     * @param address the address to send the mail to
+     */
     void sendInvitationEmail(String address);
 }
---
+++ app/src/main/java/ch/epfl/sweng/groupup/lib/email/MockGmailService.java
@@ -0,0 +1,8 @@
+package ch.epfl.sweng.groupup.lib.email;
+
+/**
+ * This class has been created for testing purpose and is used to mock
+ * a GMailService. Note that it actually does NOT send any emails!
+ */
+public class MockGmailService {
+}
