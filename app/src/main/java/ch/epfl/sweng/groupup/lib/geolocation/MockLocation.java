f5df03f95680fdcd752a28ad6843b5a1a35beaea
CedMaire <cedric.maire@epfl.ch>
2017-11-04 14:06:48 +0100

    Dependencies, permissions and skeleton for geolocation added

---
+++ app/src/main/java/ch/epfl/sweng/groupup/lib/geolocation/MockLocation.java
@@ -0,0 +1,32 @@
+package ch.epfl.sweng.groupup.lib.geolocation;
+
+import android.location.Location;
+import android.os.Bundle;
+
+public final class MockLocation implements GeoLocationInterface {
+    @Override
+    public Location getGeoLocation() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // TODO
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        // TODO
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        // TODO
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        // TODO
+    }
+}
