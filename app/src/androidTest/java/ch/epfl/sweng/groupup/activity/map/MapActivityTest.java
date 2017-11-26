b99e307368fd7e536c44568d009a028ae1afa492
Hampus Ramström <hampusram93@gmail.com>
2017-11-26 17:58:18 +0100

    Implemented testAddAndRemoveMarker test

--- app/src/androidTest/java/ch/epfl/sweng/groupup/activity/map/MapActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/groupup/activity/map/MapActivityTest.java
@@ -3,6 +3,10 @@ package ch.epfl.sweng.groupup.activity.map;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.contrib.BuildConfig;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -12,12 +16,21 @@ import ch.epfl.sweng.groupup.R;
 import ch.epfl.sweng.groupup.activity.event.creation.EventCreationActivity;
 import ch.epfl.sweng.groupup.lib.database.Database;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.is;
 
 public class MapActivityTest {
 
@@ -32,7 +45,7 @@ public class MapActivityTest {
 
     @Test
     public void ensureMapShownOnShortClick()  {
-        String name="maptest";
+        String name="maptest1";
         onView(withId(R.id.ui_edit_event_name)).perform(typeText(name));
         Espresso.closeSoftKeyboard();
 
@@ -42,5 +55,43 @@ public class MapActivityTest {
 
         onView(withContentDescription("Google Map")).perform(click());
     }
+
+    @Test
+    public void testAddAndRemoveMarker()  {
+        String name = "maptest2";
+        String nameMarker = "markertest";
+        String markeDescription = "markerdescription";
+        onView(withId(R.id.ui_edit_event_name)).perform(typeText(name));
+        Espresso.closeSoftKeyboard();
+
+        onView(withId(R.id.save_new_event_button)).perform(click());
+
+        onView(withId(R.id.linear_layout_event_list)).perform(click());
+
+        onView(withContentDescription("Google Map")).perform(longClick());
+
+        onView(withHint(R.string.poi_title_hint)).perform(typeText(nameMarker));
+        onView(withHint(R.string.poi_description_hint)).perform(typeText(markeDescription));
+
+        onView(withText(R.string.poi_create_add)).perform(click());
+
+        UiDevice uiDevice = UiDevice.getInstance(getInstrumentation());
+        UiObject mMarker1 = uiDevice.findObject(new UiSelector().descriptionContains(nameMarker));
+        try {
+            mMarker1.dragTo(mMarker1,1);
+            onView(withText(R.string.poi_remove_positive)).perform(click());
+            mMarker1.click();
+        } catch (UiObjectNotFoundException e) {
+            assertEquals(1,1);
+            return;
+        }
+        assertEquals(0,1);
+    }
+
+    // One add test
+
+    // One add and remove test
+
+    // One add but not remove test
 
 }
