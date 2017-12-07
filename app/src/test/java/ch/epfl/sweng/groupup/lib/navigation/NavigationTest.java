6765ba0619a0b8a4f688fb542bd8c93703fe1349
Xavier Pantet <xavpantet@gmail.com>
2017-12-07 15:19:56 +0100

    Fixed test for directions

--- app/src/test/java/ch/epfl/sweng/groupup/lib/navigation/NavigationTest.java
+++ app/src/test/java/ch/epfl/sweng/groupup/lib/navigation/NavigationTest.java
@@ -9,15 +9,8 @@ import java.io.IOException;
 
 public class NavigationTest {
     private static class MyLocation extends Location {
-
         private final double latitude;
         private final double longitude;
-
-        public MyLocation(String provider) {
-            super(provider);
-            latitude = 0.0;
-            longitude = 0.0;
-        }
 
         public MyLocation(double latitude, double longitude){
             super("");
@@ -38,7 +31,6 @@ public class NavigationTest {
     public void test(){
         NavigationModelInterface model = new GoogleMapsNavigationModel();
         try {
-            String provider = null;
             Location origin = new MyLocation(46.5197, 6.6323);
             Location destination = new MyLocation(47.3769, 8.5417);
             System.out.println(model.findRoute(origin, destination));
