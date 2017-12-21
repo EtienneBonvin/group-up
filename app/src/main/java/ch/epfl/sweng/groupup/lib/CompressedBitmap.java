59088d744e684b509db2bc77c6029768b49aafe1
Etienne Bonvin <etienne.bonvin@epfl.ch>
2017-12-21 18:09:36 +0100

    Comment CompressedBitmap

--- app/src/main/java/ch/epfl/sweng/groupup/lib/CompressedBitmap.java
+++ app/src/main/java/ch/epfl/sweng/groupup/lib/CompressedBitmap.java
@@ -15,7 +15,10 @@ public class CompressedBitmap {
 
     private final byte[] compressed;
 
-
+    /**
+     * Constructor for the compressed bitmap taking a bitmap as an argument.
+     * @param bitmap the bitmap the object should represent.
+     */
     public CompressedBitmap(Bitmap bitmap) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
@@ -23,21 +26,38 @@ public class CompressedBitmap {
     }
 
 
+    /**
+     * Constructor for the compressed bitmap taking a byte array as an argument.
+     * @param compressed the compressed byte array representing a bitmap.
+     */
     public CompressedBitmap(byte[] compressed) {
         this.compressed = compressed.clone();
     }
 
 
+    /**
+     * Returns the object as a Bitmap object.
+     * @return a Bitmap of the object.
+     */
     public Bitmap asBitmap() {
         return BitmapFactory.decodeByteArray(compressed, 0, compressed.length);
     }
 
 
+    /**
+     * Returns the object as a compressed byte array.
+     * @return the Bitmap as a byte array.
+     */
     public byte[] asByteArray() {
         return compressed.clone();
     }
 
 
+    /**
+     * Overrides equals method.
+     * @param o the object to be compared to.
+     * @return true if this and the object o are equals, false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
