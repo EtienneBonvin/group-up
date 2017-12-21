7229c6be2f8e0f2a4c665c0faac613a11894e19f
Xavier Pantet <xavpantet@gmail.com>
2017-12-21 17:56:21 +0100

    Auto-refreshing list of events

--- app/src/main/java/ch/epfl/sweng/groupup/object/account/Account.java
+++ app/src/main/java/ch/epfl/sweng/groupup/object/account/Account.java
@@ -71,6 +71,7 @@ public final class Account extends User implements Watchee {
             default:
                 addOrUpdateCurrentEvent(event);
         }
+        notifyAllWatchers();
         return shared;
     }
 
